dcb37b1aa8361ef3ee3d3cee96f132c5831acdde
Camon-fi07 <422nikitos244@mail.ru>
2025-06-01 22:08:10 +0700

    master - поправил модальное окно в адаптиве

--- src/modules/users/users.skeleton.tsx
+++ src/modules/users/users.skeleton.tsx
@@ -1,6 +1,7 @@
 import { Skeleton, TableCell, TableRow } from '@/shared/UI';
 
 import { COLUMNS } from './users.const';
+import styles from './users.module.css';
 
 export const TableUsersSkeleton = () =>
   [...new Array(5)].map((_, index) => (
@@ -16,9 +17,9 @@ export const TableUsersSkeleton = () =>
 export const TableUsersMobileSkeleton = () =>
   [...new Array(5)].map((_, index) => (
     <TableRow data-testid='skeleton-row' key={index}>
-      <TableCell>
+      <TableCell className={styles.tableCellMobile}>
         {[...new Array(COLUMNS.length)].map((_, index) => (
-          <Skeleton key={index} height={10} width='100%' />
+          <Skeleton key={index} height={20} width='100%' />
         ))}
       </TableCell>
     </TableRow>
--- src/shared/hooks/useIsMobile.ts
+++ src/shared/hooks/useIsMobile.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-const MOBILE_SIZE = 400;
+const MOBILE_SIZE = 480;
 
 export const useIsMobile = () => {
   const [isMobile, setIsMobile] = useState(false);
